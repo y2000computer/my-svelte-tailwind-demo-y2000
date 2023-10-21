dd1b43f3c75cd75c6d6533245ff7390417f73b53
y2000computer <y2000computer@gmail.com>
2023-10-21 15:05:47 +0100

    update listening port

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,13 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	dev:{
+	port:5173,
+	strictPort:false,
+	},
+	preview:{
+	port:80,
+	strictPort:false,
+	}
 });
